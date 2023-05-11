8e35e1bafabe90418da662593f3ea9bbb533473d
Henry <henryhuynh404@gmail.com>
2023-05-11 10:40:09 -0400

    Pushing updated class files

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.Test;
 public class Main {
