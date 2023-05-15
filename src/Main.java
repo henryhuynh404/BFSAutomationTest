964a140bdda0450de687665f5f72be0fd3181820
Henry <henryhuynh404@gmail.com>
2023-05-15 16:09:49 -0400

    Jenkins build Test

--- src/Main.java
+++ src/Main.java
@@ -23,32 +23,7 @@ public class Main {
         Assert.assertEquals(expectedUrl, currentUrl);
     }
     @Test
-    public void businessInstitutions() {
-        driver.findElement(By.xpath("//*[@id=\"main\"]/section[1]/content-lock/h-box[1]/v-box[3]/a/h-box/chooser-text/h2")).click();
-        System.out.println("Loading businessInstitutions");
-        String currentUrl = driver.getCurrentUrl();
-        String expectedUrl = "https://www.fiscal.treasury.gov/business/";
-        Assert.assertEquals(expectedUrl, currentUrl);
-    }
-    @Test
-    public void testGeneralPublic() {
-        driver.findElement(By.xpath("//*[@id=\"main\"]/section[1]/content-lock/h-box[1]/v-box[1]/a/h-box/chooser-text/h2")).click();
-        System.out.println("Loading testGeneralPublic()");
-        String currentUrl = driver.getCurrentUrl();
-        String expectedUrl = "https://www.fiscal.treasury.gov/public/";
-        Assert.assertEquals(expectedUrl, currentUrl);
-    }
-    @Test
-    public void testGovernmentAgencies() {
-        driver.navigate().back();
-        driver.findElement(By.xpath("//*[@id=\"main\"]/section[1]/content-lock/h-box[1]/v-box[2]/a/h-box/chooser-text/h2")).click();
-        System.out.println("Loading testGovernmentAgencies()");
-        String currentUrl = driver.getCurrentUrl();
-        String expectedUrl = "https://www.fiscal.treasury.gov/government/";
-        Assert.assertEquals(expectedUrl, currentUrl);
-    }
-    @Test
-    public void testSubMenu() throws InterruptedException{
+    public void testSubMenu() throws InterruptedException {
         System.out.println("Loading Sub Menus()");
         driver.findElement(By.xpath("//*[@id=\"header-site\"]/div[3]/content-lock/h-box/v-box[2]/h-box/v-box[1]/button")).click();
         Thread.sleep(500);
@@ -156,11 +131,37 @@ public class Main {
         String expectedCareerUrl = "https://www.fiscal.treasury.gov/careers/";
         Assert.assertEquals(expectedCareerUrl, currentCareerUrl);
         Thread.sleep(500);
-    }
 
+        driver.close();
+    }
+    @Test
+    public void businessInstitutions() {
+        driver.findElement(By.xpath("//*[@id=\"main\"]/section[1]/content-lock/h-box[1]/v-box[3]/a/h-box/chooser-text/h2")).click();
+        System.out.println("Loading businessInstitutions");
+        String currentUrl = driver.getCurrentUrl();
+        String expectedUrl = "https://www.fiscal.treasury.gov/business/";
+        Assert.assertEquals(expectedUrl, currentUrl);
+    }
+    @Test
+    public void testGeneralPublic() {
+        driver.findElement(By.xpath("//*[@id=\"main\"]/section[1]/content-lock/h-box[1]/v-box[1]/a/h-box/chooser-text/h2")).click();
+        System.out.println("Loading testGeneralPublic()");
+        String currentUrl = driver.getCurrentUrl();
+        String expectedUrl = "https://www.fiscal.treasury.gov/public/";
+        Assert.assertEquals(expectedUrl, currentUrl);
+    }
+    @Test
+    public void testGovernmentAgencies() {
+        driver.navigate().back();
+        driver.findElement(By.xpath("//*[@id=\"main\"]/section[1]/content-lock/h-box[1]/v-box[2]/a/h-box/chooser-text/h2")).click();
+        System.out.println("Loading testGovernmentAgencies()");
+        String currentUrl = driver.getCurrentUrl();
+        String expectedUrl = "https://www.fiscal.treasury.gov/government/";
+        Assert.assertEquals(expectedUrl, currentUrl);
+    }
     @Test
     public void jenkinsMsg() {
-        System.out.println("Jenkins Test");
+        System.out.println("Jenkins Build Test");
     }
 
 }
